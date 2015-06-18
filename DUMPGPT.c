c222a441fd5b1bab1546399476cd9b6471fe3836
sunil1117 <sunil1117@gmail.com>
2015-06-18 16:34:44 +0800

    remove some message

--- DUMPGPT.c
+++ DUMPGPT.c
@@ -1,18 +1,4 @@
-/** @file
-  Clovertrail I2C test application
 
-  This application tests "UEFI Entropy-Gathering Protocol" 
-
-Copyright (c) 2014, Intel Corporation. All rights reserved.<BR>
-This software and associated documentation (if any) is furnished
-under a license and may only be used or copied in accordance
-with the terms of the license. Except as permitted by such
-license, no part of this software or documentation may be
-reproduced, stored in a retrieval system, or transmitted in any
-form or by any means without the express written consent of
-Intel Corporation.
-
-**/
 #include <Library/MemoryAllocationLib.h>
 #include <Library/UefiLib.h>
 #include <Protocol/DiskIo.h>
